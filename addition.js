fc900aa1d096755ff54feba1704e96ff4d693350
SunethChinthaka <sunethedu@gmail.com>
2022-08-12 20:10:42 +0530

    functions impl and modify the html

--- addition.js
+++ addition.js
@@ -1,1 +1,6 @@
-function add() {}
+function add() {
+  var num1 = document.getElementById("firstNumber").value;
+  var num2 = document.getElementById("secondNumber").value;
+  var result = parseInt(num1) + parseInt(num2);
+  document.getElementById("result").value = result;
+}
--- substraction.js
+++ substraction.js
@@ -1,1 +1,6 @@
-function substraction() {}
+function substraction() {
+  var num1 = document.getElementById("firstNumber").value;
+  var num2 = document.getElementById("secondNumber").value;
+  var result = parseInt(num1) - parseInt(num2);
+  document.getElementById("result").value = result;
+}
